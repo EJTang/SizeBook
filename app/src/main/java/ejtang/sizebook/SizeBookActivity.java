79a4f17eb3423472f9e28ee248c9f73344ac2d8a
EJTang <ejtang@ualberta.ca>
2017-01-28 18:36:52 -0700

    first commit

---
+++ app/src/main/java/ejtang/sizebook/SizeBookActivity.java
@@ -0,0 +1,13 @@
+package ejtang.sizebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SizeBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_size_book);
+    }
+}
